4cd4be148e4f83297ce71e2f31728dcf0facbc60
KoHtet <kokoh6329@gmail.com>
2022-08-17 00:41:22 +0630

    Add Lazy Load

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import { Auth } from "./pages";
-import Navbar from "./components/Navbar";
+import React, { lazy, Suspense } from "react";
 import { Container } from "@mui/material";
-
 import { useSelector } from "react-redux";
 import AnimatedRoutes from "./components/AnimatedRoute";
+
+import Navbar from "./components/Navbar";
+import Loader from "./components/Loader";
+
+const Auth = lazy(() => import("./pages/Auth"));
 
 const App = () => {
   const token = useSelector((state) => state.auth.token);
 
   return token === null ? (
-    <Auth />
+    <Suspense fallback={<Loader />}>
+      <Auth />
+    </Suspense>
   ) : (
     <Container sx={{ position: "relative" }}>
       <Navbar />
--- src/components/AnimatedRoute.jsx
+++ src/components/AnimatedRoute.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { Home, ProductDetail, Cart } from "../pages";
+import Loader from "./Loader";
 import { AnimatePresence } from "framer-motion";
+
+const Home = lazy(() => import("../pages/Home"));
+const ProductDetail = lazy(() => import("../pages/ProductDetail"));
+const Cart = lazy(() => import("../pages/Cart"));
 
 const AnimatedRoutes = () => {
   const location = useLocation();
   return (
     <AnimatePresence>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
     </AnimatePresence>
   );
 };
